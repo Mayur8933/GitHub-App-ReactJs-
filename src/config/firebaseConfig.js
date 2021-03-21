ebecaad54e84da07b69d59b3e209625fa8fe35e1
Mayur8933 <mp326395@gmail.com>
2021-03-21 15:35:07 +0530

    Adding React Project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyD2mpS49K31apqtFdNKAEQFOxg_gHMzGwc",
+    authDomain: "mygitapp-f7ee5.firebaseapp.com",
+    projectId: "mygitapp-f7ee5",
+    storageBucket: "mygitapp-f7ee5.appspot.com",
+    messagingSenderId: "346026448263",
+    appId: "1:346026448263:web:b5b65c251438e9495a2f22",
+    measurementId: "G-4E8LMN4ZVL"
+  };
